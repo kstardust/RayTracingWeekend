cbc7c73f4029c25f79526934c6d009f2ec3fd72b
stardust <lql9705@gmail.com>
2025-07-20 20:25:57 +0900

    gamma

--- color.cpp
+++ color.cpp
@@ -1,10 +1,17 @@
 #include "color.h"
 #include "common.h"
 
+/* gamma 2 */
+double linear_to_gamma(double l) {
+  if (l > 0)
+    return std::sqrt(l);
+  return 0;
+}
+
 void write_color(FILE *out, const color &pixel_color) {
-  auto r = pixel_color.x();
-  auto g = pixel_color.y();
-  auto b = pixel_color.z();
+  auto r = linear_to_gamma(pixel_color.x());
+  auto g = linear_to_gamma(pixel_color.y());
+  auto b = linear_to_gamma(pixel_color.z());
 
   const interval intensity(0.000, 0.999);
   int rbyte = int(256 * intensity.clamp(r));
